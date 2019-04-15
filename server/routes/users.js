52ad52fe1f041fefbaa0055c47f3e10ec1325dd5
Leandro <lmacoretta@live.com.ar>
2019-04-15 16:23:00 -0300

    Inicie con jsonWebToken, hice la funcion de sign y el siguiente paso es agregar passport. Tambien voy a decumentar algunas cosas en el readme para recordar las funciones que tiene sign

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const JWT = require('jsonwebtoken');
+require('dotenv').config();
 
 /** Models */
 const User = require('../models/User');
+
+/**
+ * La funcion sign acepta un objeto que dentro va nuestro payload, luego una SecretOrKey que va ser nuestra clave unica codificada haciendo dificil que la puedan manipular, y por ultimo campos opcionales que puedo leer en la documentacion.
+ * el iss (issuer) identifica el emisor del token. Es opcional y es sensible a minusculas y mayusculas. Puedo poner lo que quiera.
+ * el sub (subject) es la parte central del token. Conecta al usuario con ese token. Por eso utilizo el _id, dado que el _id nunca va cambiar. Este campo es requerido, no puede ser opcional.
+ * el iat (Issued At) identifica el momento que el token fue emitido. Su valor es numerico. Es opcional al igual que iss.
+ * el exp (Expiration Time) es el tiempo que quiero que la token desaparezca. Se calcula desde el momento de la emisión. Opcional.
+*/
+
+signToken = user => {
+   return JWT.sign({
+      iss: 'loginExample',
+      sub: user.id,
+      iat: new Date().getTime(),
+      exp: new Date().setDate(new Date().getDate() + 1) //el dia actual + 1
+   }, 'authenticationExample')
+}
 
 /** Registro */
 router.post('/signUp', async (req, res) => {
@@ -21,8 +40,9 @@ router.post('/signUp', async (req, res) => {
    //Lo guardo en la DB
    await newUser.save();
 
+   const token = signToken(newUser);
    //Si esta todo ok, respondo con el token
-   res.status(200).json({user: 'El usuario ha sido creado con exito'});
+   res.status(200).json({ token });
 });
 
 
